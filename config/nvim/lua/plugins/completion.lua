3bd95d4831114ee875268934761a0cecc2236fe6
Brij Patel <brijpatel2106012@gmail.com>
2026-09-04 15:03:34 -0700

    Fixed auto completion within LazyVim

---
+++ config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,9 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = { preset = "enter" },
+    completion = {
+      list = { selection = { preselect = false, auto_insert = false } },
+    },
+  },
+}
